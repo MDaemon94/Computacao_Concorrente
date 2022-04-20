81ff990d5d28502ae31ef25e6f5317a6526a9c18
MDaemon94 <85412019+MDaemon94@users.noreply.github.com>
2022-04-19 21:08:11 -0300

    Update eleva_quadrado.c

--- eleva_quadrado.c
+++ eleva_quadrado.c
@@ -4,17 +4,18 @@
 #include <time.h>
 
 #define NTHREADS 2
+#define array[10000]
 
 typedef struct{
-    int pos_i, pos_f, vetor[];
+    int pos_i, pos_f;
 } t_Args;
 
 void *ElevaQuadrado(void *arg){
     t_Args *args = (t_Args *) arg;
     
     for(int i = args->pos_i; i < args->pos_f; i++){
-        int temp = args->vetor[i];
-        args->vetor[i] = temp*temp;
+        int temp = array[i];
+        array[i] = temp*temp;
     }
 
     pthread_exit(NULL);
@@ -22,9 +23,9 @@ void *ElevaQuadrado(void *arg){
 
 int main(){
     pthread_t tid_sistema[NTHREADS];
-    int i = 0, m = 5000, f = 10001, array[10000] = {};
+    int inicio = 0, meio = 5000, fim = 10000;
     for (int s=0; s<10000;s++){
-        array[s] = rand();
+        array[s] = s;
     }
     t_Args *arg;
 
@@ -37,22 +38,28 @@ int main(){
             arg->vetor[k] = array[k];
         }
         if (thread == 0){
-            arg->pos_i = i;
-            arg->pos_f = m;
+            arg->pos_i = inicio;
+            arg->pos_f = meio;
         }
         else{
-            arg->pos_i = m;
-            arg->pos_f = f;
+            arg->pos_i = meio;
+            arg->pos_f = fim;
         }
 
         if (pthread_create(&tid_sistema[thread], NULL, ElevaQuadrado, (void*) arg)) {
             printf("--ERRO: pthread_create()\n"); exit(-1);
         }
+        pthread_join(thread,NULL)
     }
 
     pthread_exit(NULL);
 
     for (int j=0; j < f; j++){
-        printf("%d ", arg->vetor[j]);
+        if (array[j]!=j*j){
+            printf("False")
+        }
+        if (array[j]==f*f){
+            printf("True")
+        }
     }
 }
